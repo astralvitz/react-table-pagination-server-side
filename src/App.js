07b03433c98ea0919b4152083adc024c72beeea3
Lavitz Star <astralvitz@pm.me>
2022-07-07 13:33:28 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import { FaTimes } from 'react-icons/fa';
+
+import AddTutorial from './components/AddTutorial';
+import Tutorial from './components/Tutorial';
+import TutorialList from './components/TutorialList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <a href="/tutorials" className="navbar-brand">
+            bezKoder
+          </a>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/tutorials"} className="nav-link">
+                Tutorials
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/add"} className="nav-link">
+                Add
+              </Link>
+            </li>
+          </div>
+        </nav>
+        <div className="container mt-3">
+          <Routes>
+            <Route exact path="/tutorials" element={<TutorialList />} />
+            <Route exact path="/add" element={<AddTutorial />} />
+            <Route path="/tutorials/:id" element={<Tutorial />} />
+          </Routes>
+        </div>
+      </div>
     </div>
   );
 }
